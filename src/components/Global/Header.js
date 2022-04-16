8bb895ad821b77a99f99fabe21fcd479d1e72fb3
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-04-16 23:35:37 +0600

    logout button

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -1,8 +1,12 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
     return (
         <div>
             <Navbar bg="light" expand="lg">
@@ -14,7 +18,8 @@ const Header = () => {
                             <Nav.Link as={Link} to="/">Home</Nav.Link>
                             <Nav.Link as={Link} to="/about">About</Nav.Link>
                             <Nav.Link as={Link} to="/checkout">Checkout</Nav.Link>
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            {user ? <button onClick={()=>signOut(auth)} className='border-0 rounded bg-danger text-white fw-bolder'>Log Out</button>: <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            }
 
                         </Nav>
                     </Navbar.Collapse>
