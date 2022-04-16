ecf054a54564106d581caf0e7577c11117f50ea4
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-04-17 00:05:29 +0600

    custom link done

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -4,6 +4,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
+import CustomLink from '../CustomComponents/CustomLink';
 
 const Header = () => {
     const [user, loading, error] = useAuthState(auth);
@@ -15,10 +16,10 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
-                            <Nav.Link as={Link} to="/checkout">Checkout</Nav.Link>
-                            {user ? <button onClick={()=>signOut(auth)} className='border-0 rounded bg-danger text-white fw-bolder'>Log Out</button>: <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/">Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/about">About</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/checkout">Checkout</Nav.Link>
+                            {user ? <button onClick={()=>signOut(auth)} className='border-0 rounded bg-danger text-white fw-bolder'>Log Out</button>: <Nav.Link as={CustomLink} to="/login">Login</Nav.Link>
                             }
 
                         </Nav>
