e03b472c6189a39b706652af0ddf2e4e25f1eb54
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-04-16 22:57:36 +0600

    notfound added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import About from './components/About/About';
 import Checkout from './components/Checkout/Checkout';
 import Footer from './components/Global/Footer';
 import Header from './components/Global/Header';
+import NotFound from './components/Global/NotFound';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Login/Register';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/about' element={<About></About>}></Route>
        <Route path='/checkout' element={<Checkout></Checkout>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
+       <Route path='/register' element={<Register></Register>}></Route>
+       <Route path='*' element={<NotFound></NotFound>}></Route>
      </Routes>
      <Footer></Footer>
     </div>
