088063d91c1c6550e66a1691384f7a556e25380f
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-04-16 23:10:56 +0600

    login and register design complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault()
+
+    }
     return (
         <div>
             <h2>Login</h2>
+            <Form className='w-25 mx-auto' onSubmit={handleLogin}>
+                <Form.Group className="mb-3">
+                    <Form.Control name='email' type="email" placeholder="Enter Email" />
+
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+
+                    <Form.Control name='password' type="password" placeholder="Enter Password" />
+                </Form.Group>
+
+                <Button className='w-100 mb-3' variant="success" type="submit">
+                    Login
+                </Button>
+            </Form>
+            <p>Don't have account? <Link className='text-decoration-none' to='/register'>Create an account now</Link></p>
         </div>
     );
 };
--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const handleRegister = e => {
+        e.preventDefault()
+
+    }
     return (
         <div>
             <h2>Register</h2>
+            <Form className='w-25 mx-auto' onSubmit={handleRegister}>
+                <Form.Group className="mb-3">
+                    <Form.Control name='name' type="text" placeholder="Enter Email" />
+
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Control name='email' type="email" placeholder="Enter Email" />
+
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+
+                    <Form.Control name='password' type="password" placeholder="Enter Password" />
+                </Form.Group>
+
+                <Button className='w-100 mb-3' variant="success" type="submit">
+                    Register
+                </Button>
+            </Form>
+            <p>Already have account? <Link className='text-decoration-none' to='/login'>Login now</Link></p>
         </div>
     );
 };
